34f0386928687211642d070772e14b7a114d82cc
JuandiM <juandimena34@gmail.com>
2021-09-21 20:09:36 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getFiveActors = contacts.splice(0, 5);
+
+function App(){
+  const [actors, setActors] = useState (getFiveActors);
+  let actorsArray = [...actors];
+
+  const addRandomActors = () => {
+    let randomActors = contacts[Math.floor(Math.random() * contacts.length) + 5];
+    let existContact = contacts.indexOf(randomActors);
+
+    if (randomActors) {
+      actorsArray.unshift(randomActors);
+
+      if (existContact > -1) {
+        contacts.splice(existContact, 1);
+      }
+
+      setActors(actorsArray);
+    };
+
+    const sortByPopularity = () => {
+      actorsArray.sort(function(a,b){
+        return b.popularity - a.popularity;
+      });
+
+      setActors(actorsArray);
+    };
+
+    const sortByName = () => {
+      actorsArray.sort((a, b) => a.name.localeCompare(b.name));
+      setActors(actors);
+    };
+
+    const deleteActor = (actorId) => {
+
+      const actorsNoDeleted = actors.filter((actor) => {
+        return actorId.id !== actorId;
+      });
+      setActors(actorsNoDeleted);
+      
+    };
+    return (
+      <div className="App">
+        <div className="tableDiv">
+          <h1>IronContacts</h1>
+          <div>
+            <button onClick={addRandomActors} className="btn">
+              Add Random Contact
+            </button>
+            <button onClick={sortByPopularity} className="btn">
+              Sort by popularity
+            </button>
+            <button onClick={sortByName} className="btn btn-danger">
+              Sort by name
+            </button>
+          </div>
+				<table className="tableActors">
+					<thead>
+						<tr>
+							<th> Picture </th>
+							<th> Name </th>
+							<th> Popularity </th>
+							<th> Won Oscar</th>
+							<th> Won Emmy</th>
+						</tr>
+					</thead>
+					<tbody>
+						{actors.map((actor) => {
+							return (
+								<tr key={actor.id}>
+									<td>
+										<img src={actor.pictureUrl} width="100px" height="120px" alt="Actor" />
+									</td>
+									<td> {actor.name} </td>
+
+									<td> {Number(actor.popularity).toFixed(2)}</td>
+									<td>{actor.wonOscar ? '🏆' : ''}</td>
+									<td>{actor.wonEmmy ? '🏆' : ''}</td>
+									<td>
+										<button onClick={() => {deleteActor(actor.id)}} className="btn btn-danger"
+											id={actor.id}>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+  
+  )};
 }
 
 export default App;
